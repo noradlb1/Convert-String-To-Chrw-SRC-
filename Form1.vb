4c6dd9489c9f2eb49791c9a7fbf21789a4c57326
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-26 22:40:54 +0100

    Convert-String-To-Chrw-SRC-
    Convert-String-To-Chrw-SRC-

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    '###################
+    'Coded By MONSTERMC
+    '###################
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Plz Enter Valid Text", MsgBoxStyle.Critical, "Error")
+        Else
+            TextBox2.Text = AscW(TextBox1.Text)
+        End If
+    End Sub
+End Class
